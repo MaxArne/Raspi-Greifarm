f95ad9add025d0feed3b3243ab0966055158221b
MaxArne <menos90@gmx.de>
2018-07-04 13:16:34 +0200

    Update Zeittest.c

--- Zeittest.c
+++ Zeittest.c
@@ -1,39 +1,100 @@
 #include <stdio.h>      // für printf
-//#include <wiringPi.h>   // für Pindefinition
+#include <wiringPi.h>   // für Pindefinition
 #include <time.h>       /* für clock_gettime */
+#include <sys/time.h>   // für gettimeofday()
 #include <stdint.h>     /* für uint64 definition */
 #include <stdlib.h>     /* für exit() definition */
-#include <inttypes.h>
+#include <unistd.h>
+//https://www.electronicshub.org/raspberry-pi-color-sensor-tutorial/
+// Pin Belegung in BCM Nummern
+// Zeitmessungen / Pulsebreite: http://ondrej1024.github.io/emond/
+#define S0        // direct auf Vcc
+#define S1        // direct auf Vcc
+#define S2 23
+#define S3 24
+#define sensorOut 25
+// Kalibrierungswerte für rot, blau, grün
+long Calred = 0;
+long Calblue = 0;
+long Calgreen = 0;
+long red = 0;
+
+//frequency scalling to 2%, 20%, 100%
+// S0 = Low + S1 = High -> 2%
+// S0 = High + S1 = Low -> 20%
+// S0 = High + S1 = High -> 100%
 
 int main(void)
 {
-clock_t t1,t2;
+// OUTPUT / INPUT Definition
+if (wiringPiSetup() == -1)
+    return 1;
+pinMode (S2, OUTPUT);
+pinMode (S3, OUTPUT);
+pinMode (sensorOut, INPUT);
+// loop Variablen initialisieren
 int i = 0;
-uint64_t diff = 1000000000;
-struct timespec startred, endred, startblue, endblue,startgreen, endgreen;
+long diff;     //für Nanosec
+//Zeitmessung für Pulsweite
+struct timeval startred, endred;
 
-clock_gettime(CLOCK_REALTIME, &startred);
-while(i<1000)
-{i= i+1;}
+//Auslesen der Farben Tabelle
+//S2 = Low + S3 = Low -> rot
+//S2 = Low + S3 = High -> blau
+//S2 = High + S3 = High -> grün
 
-clock_gettime(CLOCK_REALTIME, &endred);
+// Kalibrierung
+printf("Weiß Kal. nach 5 sec\n");
+delay(500);
+printf("start\n");
+fflush(stdout);
+//Start der Kalibrierung
+//rot lesen
+digitalWrite(S2,LOW);
+digitalWrite(S3,LOW);
+    
+while (i == 2)
+{
+    	
+    //Puls Messung risingedge bis fallingedge ms Messung
+    if (digitalRead (sensorOut) == 1 && i == 0)
+    {
+    gettimeofday(&startred, NULL);
+    i = 1;
+    }
+    
+    if (digitalRead (sensorOut) == 0 && i == 1)
+    {
+    gettimeofday(&endred, NULL);
+    i = 2;
+    }
+}
+Calred = (endred.tv_sec - startred.tv_sec) + (endred.tv_usec - startred.tv_usec);
+printf("Rot = %lu \n", Calred);
+    //Zähler zurücksetzen für den nächsten Loop
+    i = 0;
 
-diff = diff * (endred.tv_sec - startred.tv_sec) + (endred.tv_nsec - startred.tv_nsec);  
-printf("%llu\n",(long long unsigned int) diff);  
-float ts = startred.tv_nsec;
-float te = endred.tv_nsec;
-printf("%llu\n"",""%llu\n",(long long unsigned int) ts,(long long unsigned int) te);
-
-clock_gettime(CLOCK_REALTIME, &startred);
-while(i<10)
-{i= i+1;}
-
-clock_gettime(CLOCK_REALTIME, &endred);
-
-diff = diff * (endred.tv_sec - startred.tv_sec) + (endred.tv_nsec - startred.tv_nsec);
-printf("%llu\n",(long long unsigned int) diff);   
-ts = startred.tv_nsec;
-te = endred.tv_nsec;
-printf("%llu\n"",""%llu\n",(long long unsigned int) ts,(long long unsigned int) te);
-return(0);
+digitalWrite(S2,LOW);
+digitalWrite(S3,LOW);
+    
+while (i == 2)
+{
+    	
+    //Puls Messung risingedge bis fallingedge ms Messung
+    if (digitalRead (sensorOut) == 1 && i == 0)
+    {
+    gettimeofday(&startred, NULL);
+    i = 1;
+    }
+    
+    if (digitalRead (sensorOut) == 0 && i == 1)
+    {
+    gettimeofday(&endred, NULL);
+    i = 2;
+    }
+}
+red = (endred.tv_sec - startred.tv_sec) + (endred.tv_usec - startred.tv_usec);
+printf("Rot = %lu \n", red);
+red= red/Calred;
+printf("Rot = %lu \n", red);
 }
